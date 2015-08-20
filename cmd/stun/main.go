f59a4a4140616a351b2dc30d7804226088871ee7
Markus Tzoe <chou.marcus@gmail.com>
2015-08-20 16:37:03 +0800

    refine cmd/stun

--- cmd/stun/main.go
+++ cmd/stun/main.go
@@ -41,46 +41,42 @@ func main() {
 
 	if "" == *serverHost {
 		flag.Usage()
-		fmt.Println("\r\nERROR: You must specify the host name of the STUN server")
+		fmt.Fprintln(os.Stderr, "\r\nERROR: You must specify the host name of the STUN server")
 		os.Exit(1)
 	}
 
 	// Lookup the host name.
 	if ips, err = net.LookupHost(*serverHost); nil != err {
-		fmt.Println(fmt.Sprintf("ERROR: %s", err))
+		fmt.Fprintf(os.Stderr, "ERROR: %s", err)
 		os.Exit(1)
 	} else if 0 == len(ips) {
-		fmt.Println(fmt.Sprintf("ERROR: Can not lookup host \"%s\".", *serverHost))
+		fmt.Fprintf(os.Stderr, "ERROR: Can not lookup host \"%s\".", *serverHost)
 		os.Exit(1)
 	}
 
-	fmt.Println(fmt.Sprintf("% -15s: %s", "Host", *serverHost))
-	fmt.Println(fmt.Sprintf("% -15s: %d", "Port", *serverPort))
-	fmt.Println(fmt.Sprintf("% -15s: IP%d = %s", "IPs", 0, ips[0]))
+	fmt.Printf("% -14s: %s\n", "Host", *serverHost)
+	fmt.Printf("% -14s: %d\n", "Port", *serverPort)
+	fmt.Printf("% -14s: IP%d = %s\n", "IPs", 0, ips[0])
 	for i := 1; i < len(ips); i++ {
-		fmt.Println(fmt.Sprintf("% -15s: IP%d = %s", " ", i, ips[i]))
+		fmt.Printf("% -14s: IP%d = %s\n", " ", i, ips[i])
 	}
-	fmt.Println("\n\n")
 
 	if len(ips) > 1 {
-		fmt.Println(fmt.Sprintf("The given host name is associated to %d IP addresses.", len(ips)))
-		fmt.Println(fmt.Sprintf("Which one should I use?"))
+		fmt.Printf("The given host name is associated to %d IP addresses.\n", len(ips))
+		fmt.Printf("Which one should I use?\n")
 
 		for {
 			var response string
 			var idx int
 
-			fmt.Println(fmt.Sprintf("\nPlease, enter an integer between 0 (for IP0) and %d (for IP%d).", len(ips)-1, len(ips)))
+			fmt.Printf("\nPlease, enter an integer between 0 (for IP0) and %d (for IP%d).\n", len(ips)-1, len(ips))
 			fmt.Scanln(&response)
 
-			idx, err = strconv.Atoi(response)
-			if nil != err {
-				fmt.Println(fmt.Sprintf("The given value (%s) is invalid.", response))
+			if idx, err = strconv.Atoi(response); nil != err {
+				fmt.Printf("The given value (%s) is invalid.\n", response)
 				continue
-			}
-
-			if (idx < 0) || (idx >= len(ips)) {
-				fmt.Println(fmt.Sprintf("The given value (%d) is invalid.", idx))
+			} else if idx < 0 || idx >= len(ips) {
+				fmt.Printf("The given value (%d) is invalid.\n", idx)
 				continue
 			}
 
@@ -92,14 +88,14 @@ func main() {
 		ip, err = stun.MakeTransportAddress(ips[0], *serverPort)
 		_ = err
 	}
-	fmt.Println(fmt.Sprintf("\nUsing transport address \"%s\".\n", ip))
+	fmt.Printf("\nUsing transport address \"%s\".\n", ip)
 
 	// Perform discovery.
 	stun.ClientInit(ip)
 	stun.ActivateOutput(*verbosityLevel, nil)
 
 	if nat, err = stun.ClientDiscover(); nil != err {
-		fmt.Println(fmt.Sprintf("An error occured: %s", err))
+		fmt.Printf("An error occured: %s\n", err)
 		os.Exit(1)
 	}
 
@@ -108,22 +104,22 @@ func main() {
 
 	switch nat {
 	case stun.NAT_ERROR:
-		fmt.Println(fmt.Sprintf("Test failed: %s", err))
+		fmt.Println("Test failed:", err)
 	case stun.NAT_BLOCKED:
-		fmt.Println(fmt.Sprintf("UDP is blocked."))
+		fmt.Println("UDP is blocked.")
 	case stun.NAT_UNKNOWN:
-		fmt.Println(fmt.Sprintf("Unexpected response from the STUN server. All we can say is that we are behind a NAT."))
+		fmt.Println("Unexpected response from the STUN server. All we can say is that we are behind a NAT.")
 	case stun.NAT_FULL_CONE:
-		fmt.Println(fmt.Sprintf("We are behind a full cone NAT."))
+		fmt.Println("We are behind a full cone NAT.")
 	case stun.NAT_SYMETRIC:
-		fmt.Println(fmt.Sprintf("We are behind a symetric NAT."))
+		fmt.Println("We are behind a symetric NAT.")
 	case stun.NAT_RESTRICTED:
-		fmt.Println(fmt.Sprintf("We are behind a restricted NAT."))
+		fmt.Println("We are behind a restricted NAT.")
 	case stun.NAT_PORT_RESTRICTED:
-		fmt.Println(fmt.Sprintf("We are behind a port restricted NAT."))
+		fmt.Println("We are behind a port restricted NAT.")
 	case stun.NAT_NO_NAT:
-		fmt.Println(fmt.Sprintf("We are not behind a NAT."))
+		fmt.Println("We are not behind a NAT.")
 	case stun.NAT_SYMETRIC_UDP_FIREWALL:
-		fmt.Println(fmt.Sprintf("We are behind a symetric UDP firewall."))
+		fmt.Println("We are behind a symetric UDP firewall.")
 	}
 }
